8980497338dd2e7eb9dd1aafe353cf83fd136b88
ThaiVinh <hathaivinhs@gmail.com>
2024-04-02 23:23:54 +0700

    finish sign in 1

--- controllers/SignUpController.js
+++ controllers/SignUpController.js
@@ -1,6 +1,8 @@
 const { json } = require("express")
 const userValidation = require("../validation/userValidation")
 const UserModel = require("../models/UserModel")
+const bcrypt = require('bcrypt');
+const moment = require('moment');
 
 exports.signUp = async (req, res, next) => {
     try {
@@ -31,10 +33,26 @@ exports.signUp = async (req, res, next) => {
             });
         }
         // Hash password
-
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+      
         // Insert account into database
+        const {birthday} = req.body;
+        const newUser = await UserModel.create({
+            ...req.body,
+            birthday: moment(birthday, "DD/MM/YYYY").format(
+                "YYYY-MM-DD"
+              ),
+            password: hashedPassword,
+        })
+        
 
         // Response message to client
+        newUser.password = undefined;
+        return res.status(201).json({
+            message: "Account created successfully",
+            user: newUser
+        })
 
     } catch (error) {
         return res.status(500).json(error)
